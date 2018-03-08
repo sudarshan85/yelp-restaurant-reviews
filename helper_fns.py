96e8d6d2c0f5e4780dc32e4f205a263dcbed2e70
Sudarshan Srinivasan <sudarshan85@gmail.com>
2018-03-08 16:20:11 -0500

    Refactored some file names
    Removed helpers

--- helper_fns.py
+++ helper_fns.py
@@ -7,8 +7,42 @@ import time
 
 from spacy.lang.en.stop_words import STOP_WORDS
 
-from helpers import lemmatized_sentence_corpus, line_review
+def punct_space(token):
+    """
+    helper function to eliminate tokens
+    that are pure punctuation or whitespace
+    """
+    
+    return token.is_punct or token.is_space
 
+
+def line_review(filename):
+    """
+    generator function to read in reviews from the file
+    and un-escape the original line breaks in the text
+    """
+    
+    with codecs.open(filename, encoding='utf_8') as f:
+        for review in f:
+            yield review.replace('\\n', '\n')
+
+            
+def lemmatized_sentence_corpus(filename, spacy_model):
+    """
+    generator function to use spaCy to parse reviews,
+    lemmatize the text, and yield sentences
+    """
+    
+    for parsed_review in spacy_model.pipe(line_review(filename), batch_size=10000, n_threads=8):        
+        for sent in parsed_review.sents:
+            yield u' '.join([token.lemma_ for token in sent if not punct_space(token)])
+
+            
+def full_df(df):
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        print(df)  
+
+        
 def get_restaurant_ids(businesses_filepath):
     restaurant_ids = set()
 
@@ -29,6 +63,7 @@ def get_restaurant_ids(businesses_filepath):
 
     # turn restaurant_ids into a frozenset, as we don't need to change it anymore
     return frozenset(restaurant_ids)
+
 
 def write_review_file(review_txt_filepath, review_json_filepath, restaurant_ids):
     review_count = 0
@@ -54,6 +89,7 @@ def write_review_file(review_txt_filepath, review_json_filepath, restaurant_ids)
 
     return review_count
 
+
 def write_unigram_sents(unigram_sentences_filepath, review_txt_filepath, spacy_model):
     sentence_count = 0
     with codecs.open(unigram_sentences_filepath, 'w', encoding='utf_8') as f:
@@ -62,6 +98,7 @@ def write_unigram_sents(unigram_sentences_filepath, review_txt_filepath, spacy_m
             sentence_count += 1
             
     return sentence_count
+
 
 def write_sents(sentences_filepath, sentences, model):
     sentence_count = 0
@@ -72,6 +109,7 @@ def write_sents(sentences_filepath, sentences, model):
             sentence_count += 1
 
     return sentence_count
+
 
 def write_trigram_review(trigram_reviews_filepath, review_txt_filepath, bigram_model, trigram_model, spacy_model):
     review_count = 0
